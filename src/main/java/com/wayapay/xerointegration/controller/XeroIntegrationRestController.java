52384058474fe3f325246897d397c00134c4a56c
Edceebee <edceebee@gmail.com>
2023-05-21 13:52:48 +0100

    refactor

--- src/main/java/com/wayapay/xerointegration/controller/XeroIntegrationRestController.java
+++ src/main/java/com/wayapay/xerointegration/controller/XeroIntegrationRestController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/xero")
+@RequestMapping("/api/v1")
 public class XeroIntegrationRestController
 {
 
@@ -25,13 +25,13 @@ public class XeroIntegrationRestController
     private static final Gson JSON = new Gson();
 
 
-    @GetMapping("/bank-transaction/single")
+    @GetMapping("/transaction")
     public ResponseEntity<XeroSingleBankTransactionResponsePayload> handleGetBankTransactionRequest(@RequestParam("bankTransactionId") String bankTransactionId){
         XeroSingleBankTransactionResponsePayload responsePayload = xeroIntegrationService.getSingleBankTransaction(bankTransactionId);
         return ResponseEntity.ok(responsePayload);
     }
 
-    @PostMapping("/bank-transaction")
+    @PostMapping("/transactions")
     public ResponseEntity<XeroBankTransactionResponsePayload> handleGetBankTransactionRequest(@RequestBody XeroBankTransactionRequestPayload requestPayload){
         ValidationPayload validationPayload = modelValidator.doModelValidation(requestPayload);
         if(validationPayload.isHasError()){
